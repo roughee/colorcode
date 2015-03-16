beb23c2b1af5cd583c9ed80495ce5dbbf35fe65e
roughee <roughee@outlook.com>
2015-03-16 22:51:33 +0100

    added colors

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,10 +18,20 @@ angular.module('starter', ['ionic'])
   });
 })
 
-.controller('MainCtrl', function($scope) {
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('main', {
+    url: '/',
+    templateUrl: 'main.html',
+    controller: 'MainCtrl'
+  })
+})
+
+app.controller('MainCtrl', function($scope) {
   $scope.rows = [
-      [ { color: 'Yellow' }, { color: 'Blue' }, { color: 'Red' } ],
-      [ { color: 'Yellow' }, { color: 'Blue' }, { color: 'Red' } ],
-      [ { color: 'Yellow' }, { color: 'Blue' }, { color: 'Red' } ]
+      [ { colorText: 'Blue', color: '004358' }, { colorText: 'Electric', color: '1F8A70' }, { colorText: 'Green', color: 'BEDB39' } ],
+      [ { colorText: 'Yellow', color: 'FFE11A' }, { colorText: 'Orange', color: 'FD7400' }, { colorText: 'Red', color: 'B9121B' } ],
+      [ { colorText: 'Maroon', color: '4C1B1B' }, { colorText: 'Tan', color: 'F6E497' }, { colorText: 'Brown', color: 'BD8D46' } ]
   ];
 });
